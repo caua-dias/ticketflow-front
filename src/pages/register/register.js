5ea4237ba5459273a561ed9b7a1eb41ddae996f4
Josep <josephvinibasso@gmail.com>
2026-05-25 21:12:39 -0300

    Update register.js

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,1 +1,23 @@
-console.log("register funcionando")
+document.querySelector("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const cpf = document.getElementById("cpf").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("https://localhost:5001/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, cpf, password })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("token", data.token);
+        window.location.href = "../home/home.html";
+    } else {
+        const err = await response.json();
+        alert(err.message);
+    }
+});
